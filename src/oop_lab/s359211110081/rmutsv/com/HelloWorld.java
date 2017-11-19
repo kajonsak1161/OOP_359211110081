e574b907c872e532dc6088a96f705f03a379974e
kajonsak1161 <kajonsak1161>
2017-11-19 14:48:58 +0700

    Testing upload to github

---
+++ src/oop_lab/s359211110081/rmutsv/com/HelloWorld.java
@@ -0,0 +1,16 @@
+package oop_lab.s359211110081.rmutsv.com;
+
+public class HelloWorld {
+
+//    create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello Word,I'am John'Wick");
+
+
+    
+
+
+}//main
+
+}//class
