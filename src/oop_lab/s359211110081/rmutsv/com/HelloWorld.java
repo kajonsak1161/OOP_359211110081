2fd4fe5c00895dc8aec31a22cff03f0fda26ba5c
kajonsak1161 <kajonsak1161>
2017-11-19 15:21:36 +0700

    Edit

--- src/oop_lab/s359211110081/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110081/rmutsv/com/HelloWorld.java
@@ -7,8 +7,8 @@ public static void main(String[] args) {
 
     System.out.println("Hello Word,I'am John'Wick");
     System.out.println("Kajonsak Chairat");
-    System.out.println("Information System");
-    System.out.println("MT:RMUTSV");
+    System.out.println("Information System,MT:RMUTSV");
+
 
 
 
