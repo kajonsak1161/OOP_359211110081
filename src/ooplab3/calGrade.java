640559a6f038fc13393e7b1be030cb8513921165
LAB_COM-MT_1 <kajonsak1161@gmail.com>
2017-11-29 11:42:15 +0700

    TestLoopFor

--- src/ooplab3/calGrade.java
+++ src/ooplab3/calGrade.java
@@ -5,6 +5,7 @@
 //        score 70-79 grade B
 //        score 80-100 grade A
 
+
 package ooplab3;
 
 import java.util.Scanner;
@@ -13,19 +14,16 @@ public class calGrade {
     public  static  void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int score;
-        System.out.print("Enter your score: ");
+        System.out.print("Enter your score(0-100): ");
         score = scanner.nextInt();
 //        test condition score
+
         if (score<49) System.out.print("Your grade is F");
              else if (score<59) System.out.print("Your grade is D");
                 else if (score<69) System.out.print("Your grade is C");
                     else if (score<79) System.out.print("Your grade is B");
                         else System.out.print("Your grade in A");
-
-
-
-
-
+                        break;
 
 
 
