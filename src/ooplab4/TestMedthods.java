36815e6701b8ab9288ed48941d6de7a2a575d80f
kajonsak1161 <kajonsak1161@gmail.com>
2017-12-06 11:20:05 +0700

    TestMedthods

---
+++ src/ooplab4/TestMedthods.java
@@ -0,0 +1,35 @@
+package ooplab4;
+
+public class TestMedthods {
+    //Nethod types
+    //type 1
+    public static  void  A(){
+        System.out.println("Hello A");
+    }//A
+    //type 2
+    public static void B(int x, int y){
+        System.out.println(x+" "+y);
+    }//B
+    //type3
+    public static int C(int x,int y){
+        return x + y;
+    }//C
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        A();
+        B(5,10);
+        int x = 10, y = 20;
+        B(x, y);
+
+        //type 3 return value management
+        //1
+        int num = C(x, y);
+        System.out.println(num);
+
+
+    }//main
+
+}//class
+
+
