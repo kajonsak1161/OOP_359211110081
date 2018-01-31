8990aef3e531f7df3dd82a5d56c57ddeecff92ca
kajonsak1161 <kajonsak1161@gmail.com>
2018-01-31 11:22:37 +0700

    TriangleApp

--- src/ooplab8/TriangleApp.java
+++ src/ooplab8/TriangleApp.java
@@ -2,7 +2,7 @@ package ooplab8;
 
 public class TriangleApp {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle (0.02, 30,40);
+        Triangle triangle = new Triangle (0.5, 30,40);
         System.out.println(triangle.toString());
         System.out.println("object area: "+triangle.getArea());
     }//main
