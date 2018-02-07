fe663c7350dbe9c27bdd4fa38cc1453cd584660e
kajonsak1161 <kajonsak1161@gmail.com>
2018-02-07 10:59:45 +0700

    MyPersonApp Has-a Edit

--- src/ooplab9/MyPresonApp.java
+++ src/ooplab9/MyPresonApp.java
@@ -6,6 +6,9 @@ public class MyPresonApp {
         new Address("70","NakonSriThammarat","80110"),
         new Job("manager",20000));
         System.out.println(person.toString());
+
+        person.getJob().setSalary(30000);
+        System.out.println("Name:"+person.getName()+"  Sarary:"+person.getJob().getSalary());
     }
 
 }
