c17bcb7fac4f40ab35764d6db62f10cab0f2b7b2
kajonsak1161 <kajonsak1161@gmail.com>
2018-02-07 09:20:06 +0700

    defual

--- src/ooplab8/Pet.java
+++ src/ooplab8/Pet.java
@@ -1,6 +1,7 @@
 package ooplab8;
 
 public abstract class Pet {
+    String msg = "Hello";
     private String name;
     private String age;
     //constructor
@@ -11,7 +12,7 @@ public abstract class Pet {
     }
 
     //abstract method
-    public abstract void makeNoise();
+    protected abstract void makeNoise();
 
     //toString
     @Override
