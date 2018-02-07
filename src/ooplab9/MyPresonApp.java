8e699e9ac5a963df2ab2c2321acbdffc555d8fa9
kajonsak1161 <kajonsak1161@gmail.com>
2018-02-07 10:51:01 +0700

    TestVariable

--- src/ooplab9/MyPresonApp.java
+++ src/ooplab9/MyPresonApp.java
@@ -3,7 +3,7 @@ package ooplab9;
 public class MyPresonApp {
     public static void main(String[] args) {
         Person person = new Person("001", "Kajonsak",
-        new Address("70","Na","80110"),
+        new Address("70","NakonSriThammarat","80110"),
         new Job("manager",20000));
         System.out.println(person.toString());
     }
